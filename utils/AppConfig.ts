5e8d3365005f8b31a2a954fe8a4f014e833ad36d
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-10-11 17:01:42 +0300

    chore: add linting

--- utils/AppConfig.ts
+++ utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Module CSS',
+  site_name: 'Portfolio',
+  title: 'My Landing Page',
+  description: 'Starter code for my Nextjs Boilerplate Landing Page with Module CSS',
   locale: 'ru',
 };
