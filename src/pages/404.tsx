d258cab09230d01d9783cc9066ff7327c932fd3e
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-10-14 21:36:52 +0300

    feat: add page 404 & redirect to main

---
+++ src/pages/404.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+// import Heading from '../components/Heading';
+// import styles from '../styles/404.module.scss';
+
+const Error = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    let timerId = setTimeout(() => {
+      router.push("/");
+    }, 500);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [router]);
+
+  return (
+    <div>
+      <Head>
+        <title>Error</title>
+      </Head>
+      <div>
+        <h1>404</h1>
+        <h2>Something is going wrong...</h2>
+        {/* <Heading text='404' />
+        <Heading tag='h2' text='Something is going wrong...' /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Error;
