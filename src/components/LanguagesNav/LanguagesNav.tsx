6f7e31ee2e44b0ac37b2e3b51642bcaaa85f4e7a
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-10-14 21:19:25 +0300

    feat: add active menu routing

--- src/components/LanguagesNav/LanguagesNav.tsx
+++ src/components/LanguagesNav/LanguagesNav.tsx
@@ -1,14 +1,20 @@
+import React from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import { AppLanguage, appLanguages } from "../../utils/Locale.config";
 import { LanguagesNavProps } from "./LanguagesNav.props";
+import styles from "./LanguagesNav.module.scss";
 
 export const LanguagesNav = ({ ...props }: LanguagesNavProps): JSX.Element => {
+  const { pathname } = useRouter();
   return (
     <nav {...props}>
       {Object.values(appLanguages).map((lang: AppLanguage): JSX.Element => {
         return (
           <Link key={lang.lang} href={lang.path}>
-            <a>{lang.lang}</a>
+            <a className={pathname === lang.path ? styles.active : ""}>
+              {lang.lang}
+            </a>
           </Link>
         );
       })}
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,14 +1,19 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
 import { sections } from "../../utils/App.config";
 import { NavigationProps } from "./Navigation.props";
+import styles from "./Navigation.module.scss";
 
 export const Navigation = ({ ...props }: NavigationProps): JSX.Element => {
+  const { pathname } = useRouter();
   return (
     <nav {...props}>
       {sections.map(({ id, sectionName }) => {
         return (
           <Link key={id} href={`#${id}`}>
-            <a>{sectionName}</a>
+            <a className={pathname === id ? styles.active : ""}>
+              {sectionName}
+            </a>
           </Link>
         );
       })}
