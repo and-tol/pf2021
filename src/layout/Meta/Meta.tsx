6be4ccc0ff70fbe30697873909dc7d58ce6e1d3d
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-10-21 20:32:53 +0300

    refactor: Meta

--- src/layout/Meta/Meta.tsx
+++ src/layout/Meta/Meta.tsx
@@ -15,6 +15,16 @@ export const Meta = () => {
         <meta name="keywords" content={AppConfig.keywords} />
         <meta name="description" content={AppConfig.description} />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&family=Lora:wght@400;500;600&display=swap"
+          rel="stylesheet"
+        />
       </Head>
     </>
   );
