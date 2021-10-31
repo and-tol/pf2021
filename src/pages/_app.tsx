91cbb2b84a581054bbac7cc90c7649f36b105363
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-10-31 18:44:20 +0200

    refact: some change with component Meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
+import React from "react";
+import type { AppProps } from "next/app";
 // import "sanitize.css";
 import "../styles/globals.scss";
-import type { AppProps } from "next/app";
+import { Meta } from "../layout";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Meta />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 // ?   Раскомментируйте этот метод только в том случае, если у вас есть требования к блокировке данных для каждой отдельной страницы в вашем приложении. Это отключает возможность автоматической статической оптимизации, в результате чего каждая страница в вашем приложении отображается на стороне сервера.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Head from "next/head";
 import { Hero } from "../components";
-import { Meta } from "../layout";
-
 import { Layout } from "../layout/Layout";
 import { AppConfig } from "../utils/App.config";
 
 export default function Index(): JSX.Element {
   return (
     <Layout>
-      <Meta />
       <Head>
         <title> {AppConfig.title} | About</title>
       </Head>
