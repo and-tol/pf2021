18d7d88c78dead91c56d5acccf9db67648fdd896
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-10-31 15:15:35 +0200

    refact: component LanguagesNav

--- src/components/LanguagesNav/LanguagesNav.tsx
+++ src/components/LanguagesNav/LanguagesNav.tsx
@@ -1,40 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import cn from "classnames";
-import { useRouter } from "next/router";
+import router, { useRouter } from "next/router";
 import Link from "next/link";
-import { AppLanguage, appLanguages } from "../../utils/Locale.config";
+import {
+  AppLanguage,
+  appLanguages,
+  LanguagesKeys,
+} from "../../utils/Locale.config";
 import { LanguagesNavProps } from "./LanguagesNav.props";
 import stylesLang from "./LanguagesNav.module.scss";
 import styles from "../../styles/Navigation.module.scss";
+import stylesDropdown from "./LanguagesNav.module.scss";
 import NavItemDots from "../../assets/horDots.svg";
 
 export const LanguagesNav = ({ ...props }: LanguagesNavProps): JSX.Element => {
   const { pathname } = useRouter();
+  const languages = Object.values(appLanguages);
+
+  const [currentLang, setCurrentLang] = useState<string>("eng");
+  const [visible, setVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    languages.forEach((element: AppLanguage) => {
+      if (element.path === pathname) {
+        setCurrentLang(element.lang);
+      }
+    });
+  }, [pathname]);
+
+  const handleVisible = (): void => {
+    setVisible(!visible);
+  };
+
+  const handleLangPage = (e: MouseEvent, path: string): void => {
+    e.preventDefault();
+    setVisible(!visible);
+    router.push(path);
+  };
+
   return (
-    <nav className={cn(styles.nav, stylesLang.navLang)} {...props}>
-      {Object.values(appLanguages).map(
-        (lang: AppLanguage, idx: number): JSX.Element => {
-          return (
-            <Link key={lang.lang} href={lang.path}>
-              <>
-                <a
-                  className={cn(
-                    styles.navItem,
-                    pathname === lang.path ? styles.active : undefined
-                  )}
-                >
-                  {lang.lang}
-                </a>
-                {idx !== Object.values(appLanguages).length - 1 ? (
-                  <NavItemDots className={styles.dots} />
-                ) : (
-                  <></>
-                )}
-              </>
-            </Link>
-          );
-        }
+    <div>
+      {!visible && (
+        <div className={stylesLang.navLang}>
+          <button
+            className={cn(
+              "font-accent",
+              styles.navItem,
+              styles.active,
+              // pathname === lang.path ? styles.active : undefined,
+              stylesDropdown.btn
+            )}
+            onClick={handleVisible}
+          >
+            {currentLang}
+          </button>
+        </div>
       )}
-    </nav>
+      {visible && (
+        <nav
+          className={cn(stylesLang.navLang, stylesDropdown.dropdownNav)}
+          {...props}
+        >
+          {languages.map((lang: AppLanguage, idx: number): JSX.Element => {
+            return (
+              <Link key={lang.lang} href={lang.path}>
+                <>
+                  <a
+                    className={cn(
+                      styles.navItem,
+                      pathname === lang.path ? styles.active : undefined
+                    )}
+                    onClick={(e) => handleLangPage(e, lang.path)}
+                  >
+                    {lang.lang}
+                  </a>
+                  {idx !== Object.values(appLanguages).length - 1 ? (
+                    <NavItemDots className={styles.dots} />
+                  ) : (
+                    <></>
+                  )}
+                </>
+              </Link>
+            );
+          })}
+        </nav>
+      )}
+    </div>
   );
 };
