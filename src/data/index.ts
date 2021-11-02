502f0e1da4ee78a686b4cfba716d673cc35505f1
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-11-02 18:21:51 +0200

    feat: add in api projects response

---
+++ src/data/index.ts
@@ -0,0 +1,24 @@
+import { Project } from "../interfaces";
+
+export const projects: Project[] = [
+  {
+    id: "0",
+    name: "Артём Пицца",
+    alias: "artem-pizza",
+    path: "",
+    smallImage: "artem-pizza.png",
+    image: "artem-pizza.png",
+    tehnologies: ["React", "Redux"],
+    desc: "SPA",
+  },
+  {
+    id: "0",
+    name: "Артём Пицца2",
+    alias: "artem-pizza2",
+    path: "",
+    smallImage: "artem-2.png",
+    image: "artem-pizza2.png",
+    tehnologies: ["React", "Redux"],
+    desc: "SPA",
+  },
+];
---
+++ src/interfaces/index.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  id: string;
+  name: string;
+  alias: string;
+  path: string;
+  smallImage: string;
+  image: string;
+  tehnologies: string[];
+  desc: string;
+}
