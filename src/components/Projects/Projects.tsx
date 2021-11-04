03fd408dd01716e967e328bed21b4ac5f5a2fef5
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-11-04 11:02:07 +0200

    feat: create request to server from projects page

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,47 +1,22 @@
 import cn from "classnames";
 import React from "react";
-import { GetStaticProps } from "next";
 import { Heading, ProjectLink } from "..";
 import { ProjectsProps } from "./Projects.props";
 import styles from "./Projects.module.scss";
 
-export const Projects = ({
-  projects,
-  ...props
-}: ProjectsProps): JSX.Element => {
-  console.log("projects:", projects);
-
-  const project = {
-    _id: "0",
-    name: "Артём Пицца",
-    alias: "artem-pizza",
-    path: "",
-    imageSmall: "artem-pizza.png",
-    image: "artem-pizza.png",
-    technologies: ["React", "Redux"],
-    description: "SPA",
-  };
-
+export const Projects = ({ projects }: ProjectsProps): JSX.Element => {
   return (
-    <section className={cn("wrapper", styles.projects)} {...props}>
+    <section className={cn("wrapper", styles.projects)}>
       <Heading tag="h2" className={styles.title}>
-        My Projects{" "}
+        My Projects
       </Heading>
-      <nav {...props}>
-        <ProjectLink project={project} />
-        {/* <ProjectLink project={project} /> */}
+      <nav>
+        {projects &&
+          projects.map((project) => {
+            console.log("project", project);
+            return <ProjectLink key={project._id} project={project} />;
+          })}
       </nav>
     </section>
   );
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch(`http://localhost:3005/data`);
-  const data = (await response.json()) || [];
-
-  return {
-    props: {
-      projects: data,
-    },
-  };
 };
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,17 +1,42 @@
 import React from "react";
 import Head from "next/head";
-import { Heading, Projects } from "../components";
+import cn from "classnames";
+import { Projects } from "../components";
 import { Layout } from "../layout/Layout";
 import { AppConfig } from "../utils/App.config";
+import { GetStaticProps } from "next";
+import { Project } from "../interfaces";
 
-export default function Index(): JSX.Element {
+export default function Index({ projects }: ProjectsProps): JSX.Element {
+  console.log("p", projects);
   return (
     <Layout>
       <Head>
         <title> {AppConfig.title} | Projects</title>
       </Head>
 
-      <Projects />
+      <Projects projects={projects} />
     </Layout>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch(`http://localhost:3005/api/projects`);
+  const projects = await response.json();
+
+  if (!projects) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      projects,
+    },
+  };
+};
+
+interface ProjectsProps extends Record<string, unknown> {
+  projects: Project[];
 }
