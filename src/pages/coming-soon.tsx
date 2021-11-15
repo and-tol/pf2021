384ff138f070ddd7f0835c81a80b96792b971ecf
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-11-15 19:59:48 +0200

    feat: create page cooming-soon

---
+++ src/pages/coming-soon.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import React from 'react';
+import { Heading } from '../components';
+
+const Error = (): JSX.Element => {
+  const router = useRouter();
+
+  useEffect(() => {
+    let timerId = setTimeout(() => {
+      router.push('/');
+    }, 4000);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [router]);
+
+  return (
+    <div className="wrapper center">
+      <Head>
+        <title>Coming Soon</title>
+      </Head>
+      <div>
+        <Heading tag="h1">Coming Soon</Heading>
+        <Heading tag="h2">In Developing</Heading>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
