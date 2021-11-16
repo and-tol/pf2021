70a9c7856041806526260108305281e622115a82
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-11-16 20:05:54 +0200

    fix: rename path comingsoon

--- src/pages/api/projects/index.ts
+++ src/pages/api/projects/index.ts
@@ -3,10 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { projects } from '../../../data';
 import { IProject } from '../../../interfaces/project.interface';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<IProject[]>
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     res.status(200).json(projects);
   }
